4736b31ec6acef42d9bae3fa6e6664b4a0f27efc
Evan Goertzen <evan.goertzen@gmail.com>
2023-04-22 11:42:33 -0700

    basic ui

--- app/src/main/java/com/njbrady/transmute/MainActivity.kt
+++ app/src/main/java/com/njbrady/transmute/MainActivity.kt
@@ -1,16 +1,23 @@
 package com.njbrady.transmute
 
+import android.graphics.fonts.FontStyle
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.njbrady.transmute.ui.theme.TransmuteTheme
+
+const val APP_NAME = "PacketMap"
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +29,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Layout()
                 }
             }
         }
@@ -40,4 +47,34 @@ fun DefaultPreview() {
     TransmuteTheme {
         Greeting("Android")
     }
+}
+
+@Composable
+fun Layout(){
+    LazyColumn(horizontalAlignment = Alignment.CenterHorizontally) {
+        item{
+            Title()
+            RandoButton()
+            RandoImage()
+        }
+    }
+}
+
+@Composable
+fun Title(){
+    Text(APP_NAME, fontSize = 50.sp)
+}
+
+@Composable
+fun RandoButton(){
+    Button(onClick = {
+        //your onclick code here
+    }) {
+        Text(text = "Generate Random Image", fontSize = 30.sp)
+    }
+}
+
+@Composable
+fun RandoImage(){
+    //get request goes here
 }
